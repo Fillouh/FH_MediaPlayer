97f17a49a720abbd4b26fa52ed8779f71be23f94
Filippo Leonelli <300717@studenti.unimore.it>
2024-09-18 18:33:10 +0200

    Bug fixing for jar file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.fillouh.fh_mediaplayer {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires java.desktop;
 
 
     opens org.fillouh.fh_mediaplayer to javafx.fxml;
---
+++ src/main/java/org/fillouh/fh_mediaplayer/Launcher.java
@@ -0,0 +1,10 @@
+package org.fillouh.fh_mediaplayer;
+
+import javafx.application.Application;
+import org.fillouh.fh_mediaplayer.App;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(App.class,args);
+    }
+}
